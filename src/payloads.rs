30842e45417c70de3847fe04880f0af4f65b4a08
Manuel Doncel Martos <manueldoncelmartos@gmail.com>
2025-06-22 12:24:33 +0200

    adding real example for star

--- src/payloads.rs
+++ src/payloads.rs
@@ -1,3 +1,4 @@
+use chrono::{DateTime, Utc};
 use serde::Deserialize;
 use std::time::SystemTime;
 
@@ -28,7 +29,7 @@ pub struct StarPayload {
     action: StarAction,
     repository: Repository,
     sender: GithubUser,
-    starred_at: Option<SystemTime>,
+    starred_at: Option<DateTime<Utc>>,
 }
 
 impl WebhookPayload for StarPayload {}
@@ -58,6 +59,7 @@ impl WebhookPayload for ForkPayload {}
 mod tests {
     use super::*;
     use crate::payloads::StarAction::Created;
+    use serde_json::Error;
 
     #[test]
     fn deserialize_star_payload() {
@@ -145,5 +147,145 @@ mod tests {
             sender,
         };
         assert_eq!(actual, expected)
+    }
+
+    #[test]
+    fn real_star_example_test() {
+        let json = r#"
+        {
+            "action": "created",
+            "starred_at": "2025-06-22T09:56:51Z",
+            "repository": {
+                "id": 1005506273,
+                "node_id": "R_kgDOO-7O4Q",
+                "name": "motivate-me",
+                "full_name": "manuelarte/motivate-me",
+                "private": false,
+                "owner": {
+                    "login": "manuelarte",
+                    "id": 5348899,
+                    "node_id": "MDQ6VXNlcjUzNDg4OTk=",
+                    "avatar_url": "https://avatars.githubusercontent.com/u/5348899?v=4",
+                    "gravatar_id": "",
+                    "url": "https://api.github.com/users/manuelarte",
+                    "html_url": "https://github.com/manuelarte",
+                    "followers_url": "https://api.github.com/users/manuelarte/followers",
+                    "following_url": "https://api.github.com/users/manuelarte/following{/other_user}",
+                    "gists_url": "https://api.github.com/users/manuelarte/gists{/gist_id}",
+                    "starred_url": "https://api.github.com/users/manuelarte/starred{/owner}{/repo}",
+                    "subscriptions_url": "https://api.github.com/users/manuelarte/subscriptions",
+                    "organizations_url": "https://api.github.com/users/manuelarte/orgs",
+                    "repos_url": "https://api.github.com/users/manuelarte/repos",
+                    "events_url": "https://api.github.com/users/manuelarte/events{/privacy}",
+                    "received_events_url": "https://api.github.com/users/manuelarte/received_events",
+                    "type": "User",
+                    "user_view_type": "public",
+                    "site_admin": false
+                },
+                "html_url": "https://github.com/manuelarte/motivate-me",
+                "description": "A way to motivate me to continue doing code development!",
+                "fork": false,
+                "url": "https://api.github.com/repos/manuelarte/motivate-me",
+                "forks_url": "https://api.github.com/repos/manuelarte/motivate-me/forks",
+                "keys_url": "https://api.github.com/repos/manuelarte/motivate-me/keys{/key_id}",
+                "collaborators_url": "https://api.github.com/repos/manuelarte/motivate-me/collaborators{/collaborator}",
+                "teams_url": "https://api.github.com/repos/manuelarte/motivate-me/teams",
+                "hooks_url": "https://api.github.com/repos/manuelarte/motivate-me/hooks",
+                "issue_events_url": "https://api.github.com/repos/manuelarte/motivate-me/issues/events{/number}",
+                "events_url": "https://api.github.com/repos/manuelarte/motivate-me/events",
+                "assignees_url": "https://api.github.com/repos/manuelarte/motivate-me/assignees{/user}",
+                "braches_url": "https://api.github.com/repos/manuelarte/motivate-me/branches{/branch}",
+                "tags_url": "https://api.github.com/repos/manuelarte/motivate-me/tags",
+                "blobs_url": "https://api.github.com/repos/manuelarte/motivate-me/git/blobs{/sha}",
+                "git_tags_url": "https://api.github.com/repos/manuelarte/motivate-me/git/tags{/sha}",
+                "git_refs_url": "https://api.github.com/repos/manuelarte/motivate-me/git/refs{/sha}",
+                "trees_url": "https://api.github.com/repos/manuelarte/motivate-me/git/trees{/sha}",
+                "statuses_url": "https://api.github.com/repos/manuelarte/motivate-me/statuses/{sha}",
+                "languages_url": "https://api.github.com/repos/manuelarte/motivate-me/languages",
+                "stargazers_url": "https://api.github.com/repos/manuelarte/motivate-me/stargazers",
+                "contributors_url": "https://api.github.com/repos/manuelarte/motivate-me/contributors",
+                "subscribers_url": "https://api.github.com/repos/manuelarte/motivate-me/subscribers",
+                "subscription_url": "https://api.github.com/repos/manuelarte/motivate-me/subscription",
+                "commits_url": "https://api.github.com/repos/manuelarte/motivate-me/commits{/sha}",
+                "git_commits_url": "https://api.github.com/repos/manuelarte/motivate-me/git/commits{/sha}",
+                "comments_url": "https://api.github.com/repos/manuelarte/motivate-me/comments{/number}",
+                "issue_comment_url": "https://api.github.com/repos/manuelarte/motivate-me/issues/comments{/number}",
+                "contents_url": "https://api.github.com/repos/manuelarte/motivate-me/contents/{+path}",
+                "compare_url": "https://api.github.com/repos/manuelarte/motivate-me/compare/{base}...{head}",
+                "merges_url": "https://api.github.com/repos/manuelarte/motivate-me/merges",
+                "archive_url": "https://api.github.com/repos/manuelarte/motivate-me/{archive_format}{/ref}",
+                "downloads_url": "https://api.github.com/repos/manuelarte/motivate-me/downloads",
+                "issues_url": "https://api.github.com/repos/manuelarte/motivate-me/issues{/number}",
+                "pulls_url": "https://api.github.com/repos/manuelarte/motivate-me/pulls{/number}",
+                "milestones_url": "https://api.github.com/repos/manuelarte/motivate-me/milestones{/number}",
+                "notifications_url": "https://api.github.com/repos/manuelarte/motivate-me/notifications{?since,all,participating}",
+                "labels_url": "https://api.github.com/repos/manuelarte/motivate-me/labels{/name}",
+                "releases_url": "https://api.github.com/repos/manuelarte/motivate-me/releases{/id}",
+                "deployments_url": "https://api.github.com/repos/manuelarte/motivate-me/deployments",
+                "created_at": "2025-06-20T10:33:30Z",
+                "updated_at": "2025-06-22T09:56:52Z",
+                "pushed_at": "2025-06-22T05:25:56Z",
+                "git_url": "git://github.com/manuelarte/motivate-me.git",
+                "ssh_url": "git@github.com:manuelarte/motivate-me.git",
+                "clone_url": "https://github.com/manuelarte/motivate-me.git",
+                "svn_url": "https://github.com/manuelarte/motivate-me",
+                "homepage": null,
+                "size": 14,
+                "stargazers_count": 1,
+                "watchers_count": 1,
+                "language": "Shell",
+                "has_issues": true,
+                "has_projects": true,
+                "has_downloads": true,
+                "has_wiki": false,
+                "has_pages": false,
+                "has_discussions": false,
+                "forks_count": 0,
+                "mirror_url": null,
+                "archived": false,
+                "disabled": false,
+                "open_issues_count": 0,
+                "license": null,
+                "allow_forking": true,
+                "is_template": false,
+                "web_commit_signoff_required": false,
+                "topics": [],
+                "visibility": "private",
+                "forks": 0,
+                "open_issues": 0,
+                "watchers": 1,
+                "default_branch": "main"
+            },
+            "sender": {
+                "login": "manuelarte",
+                "id": 5348899,
+                "node_id": "MDQ6VXNlcjUzNDg4OTk=",
+                "avatar_url": "https://avatars.githubusercontent.com/u/5348899?v=4",
+                "gravatar_id": "",
+                "url": "https://api.github.com/users/manuelarte",
+                "html_url": "https://github.com/manuelarte",
+                "followers_url": "https://api.github.com/users/manuelarte/followers",
+                "following_url": "https://api.github.com/users/manuelarte/following{/other_user}",
+                "gists_url": "https://api.github.com/users/manuelarte/gists{/gist_id}",
+                "starred_url": "https://api.github.com/users/manuelarte/starred{/owner}{/repo}",
+                "subscriptions_url": "https://api.github.com/users/manuelarte/subscriptions",
+                "organizations_url": "https://api.github.com/users/manuelarte/orgs",
+                "repos_url": "https://api.github.com/users/manuelarte/repos",
+                "events_url": "https://api.github.com/users/manuelarte/events{/privacy}",
+                "received_events_url": "https://api.github.com/users/manuelarte/received_events",
+                "type": "User",
+                "user_view_type": "public",
+                "site_admin": false
+            }
+        }
+        "#;
+        let actual: Result<StarPayload, Error> = serde_json::from_str(json);
+        match &actual {
+            Err(e) => {
+                println!("{}", e)
+            }
+            _ => {}
+        }
+        assert!(actual.is_ok())
     }
 }
