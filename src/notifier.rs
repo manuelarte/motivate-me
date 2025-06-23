d4ecc065dd53ac4972dcaeb22fb3cd09382612db
Manuel Doncel Martos <manueldoncelmartos@gmail.com>
2025-06-23 19:38:21 +0200

    clippy

--- src/notifier.rs
+++ src/notifier.rs
@@ -1,6 +1,4 @@
 use std::fmt::Debug;
-use std::thread;
-use std::time::Duration;
 
 pub trait Notifier: Send + Sync + Debug + 'static {
     fn sign(&self, event: String) -> Result<(), NotifierError>;
